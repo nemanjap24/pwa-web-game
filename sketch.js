3b940c24b4abc3adda9625533b4f43e808f44bd5
Kefka-dev <sith.patri@gmail.com>
2024-12-17 17:22:41 +0100

    keyboard movement, no boundaries for now

--- sketch.js
+++ sketch.js
@@ -1,14 +1,51 @@
-//runs once
+let posX = 200;
+let posY = 200;
+
+// object to track which keys are currently pressed
+let keys = {};
+
 function setup() {
   //creates canvas with dimensions of the window
   createCanvas(windowWidth/2, windowHeight/2);
-  //sets background color
-  background("#9dacff");
+  
+  // event listeners for keyPressed and keyReleased
+  window.addEventListener('keydown', handleKeyDown);
+  window.addEventListener('keyup', handleKeyUp);
 }
 
-//refreshes 60times per second
+// refreshes 60 times per second
 function draw() {
+  // clear the background
+  background("#9dacff");
   
-  //draws circle on mouse position
-  circle(mouseX,mouseY,100);
+  // checks the 
+  if (keys['w'] || keys['arrowup']) {
+    posY -= 5;
+  }
+  if (keys['a'] || keys['arrowleft']) {
+    posX -= 5;
+  }
+  if (keys['s'] || keys['arrowdown']) {
+    posY += 5;
+  }
+  if (keys['d'] || keys['arrowright']) {
+    posX += 5;
+  }
+  
+  // draw the circle
+  circle(posX, posY, 100);
+}
+
+// handle key down events
+function handleKeyDown(event) {
+  // creates a property for each key if in does not exist and sets it true
+  keys[event.key.toLowerCase()] = true;
+  console.log(keys);
+}
+
+// handle key up events
+function handleKeyUp(event) {
+  // on realease sets key to false as its no longer pressed
+  keys[event.key.toLowerCase()] = false;
+  console.log(keys);
 }
