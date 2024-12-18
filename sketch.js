f325833fcfdffd7af8e237f94455485f8c18f190
Nemanja <npolic2@gmail.com>
2024-12-18 18:50:21 +0100

    feat: update levels, change level

--- sketch.js
+++ sketch.js
@@ -1,5 +1,9 @@
 let levelsData;
 let levelData;
+let currentDifficulty = "easy";
+let currentLevelIndex = 0;
+let difficulties;
+let levelLabel;
 
 function preload() {
   levelsData = loadJSON("levels.json");
@@ -8,17 +12,23 @@ function preload() {
 function setup() {
   let canvas = createCanvas(400, 400);
   canvas.parent("canvas-container");
+  noLoop();
 
-  let levelsArray = Object.values(levelsData);
+  difficulties = Object.values(levelsData);
+  loadLevel(currentDifficulty, currentLevelIndex);
 
-  let easyLevels = levelsArray.find((difficulty) => difficulty.name === "easy");
-  levelData = easyLevels.levels.find((level) => level.level === 1);
+  levelLabel = document.getElementById("level-label");
+  updateLevelLabel();
+
+  document.getElementById("prev-level").addEventListener("click", prevLevel);
+  document.getElementById("next-level").addEventListener("click", nextLevel);
 }
 
 function draw() {
   background("#3f992f");
   let map = levelData.map;
   let tileSize = width / levelData.dimensions;
+
   for (let i = 0; i < map.length; i++) {
     for (let j = 0; j < map[i].length; j++) {
       let cell = map[i][j];
@@ -36,5 +46,33 @@ function draw() {
         circle(x + tileSize / 2, y + tileSize / 2, tileSize * 0.8);
       }
     }
+  }
+}
+
+function loadLevel(difficultyName, levelIndex) {
+  let difficulty = difficulties.find((d) => d.name === difficultyName);
+  levelData = difficulty.levels[levelIndex];
+}
+
+function updateLevelLabel() {
+  levelLabel.textContent = `Difficulty: ${currentDifficulty}, Level: ${levelData.level}`;
+}
+
+function prevLevel() {
+  if (currentLevelIndex > 0) {
+    currentLevelIndex--;
+    loadLevel(currentDifficulty, currentLevelIndex);
+    updateLevelLabel();
+    redraw();
+  }
+}
+
+function nextLevel() {
+  let difficulty = difficulties.find((d) => d.name === currentDifficulty);
+  if (currentLevelIndex < difficulty.levels.length - 1) {
+    currentLevelIndex++;
+    loadLevel(currentDifficulty, currentLevelIndex);
+    updateLevelLabel();
+    redraw();
   }
 }
