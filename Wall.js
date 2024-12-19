6c889ddfbbc0ed4d552ba0c26ab71232c6cfbb85
Kefka-dev <sith.patri@gmail.com>
2024-12-19 19:20:21 +0100

    feat: color option

--- Wall.js
+++ Wall.js
@@ -1,13 +1,15 @@
 class Wall{
-    constructor(x,y,w,h){
+    constructor(x,y,w,h, color){
         this.x = x;
         this.y = y;
         this.w = w;
         this.h = h;
+        this.color = color || "#FFFFFF"; // Default white if no color provided
     }
 
     display(){
         rectMode(CENTER);
+        fill(this.color);
         rect(this.x, this.y, this.w, this.h);
     }
 }
